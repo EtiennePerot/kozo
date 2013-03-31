3a47fa9d46d4f1a5f7f81f40a67084ef8989f967
Etienne Perot <etienne@perot.me>
2013-03-30 23:52:22 -0400

    Switch config file format to YAML, which is much easier to digest

--- src/kozo/__main__.py
+++ src/kozo/__main__.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import json
+import yaml
 if len(sys.argv) != 3:
 	sys.stderr.write('Usage: ' + sys.argv[0] + ' config.json localNode\n')
 	sys.exit(1)
@@ -8,7 +8,7 @@ if not os.path.isfile(sys.argv[1]):
 	sys.stderr.write('Config file does not exist: ' + sys.argv[1] + '\n')
 	sys.exit(2)
 try:
-	config = json.load(open(sys.argv[1], 'r'))
+	config = yaml.load(open(sys.argv[1], 'r').read(-1))
 except:
 	sys.stderr.write('Invalid JSON file: ' + sys.argv[1] + '\n')
 	sys.exit(3)
